2346e8e6bd6ff3f21db39b87a627d668ca0df666
RDCoder8 <mojinr@gmail.com>
2023-10-16 20:56:39 -0400

    Logic for the game objects when they touch

--- game.js
+++ game.js
@@ -52,5 +52,30 @@ const reset = function () {
     monster.y = 32 + (Math.random() * (canvas.height - 64))
 }
 
+// Update game objects
+const update = function (modifier) {
+	if (38 in keysDown) { // Player holding up
+		hero.y -= hero.speed * modifier;
+	}
+	if (40 in keysDown) { // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if (37 in keysDown) { // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+		hero.x += hero.speed * modifier;
+	}
 
+	// Are they touching?
+	if (
+		hero.x <= (monster.x + 32)
+		&& monster.x <= (hero.x + 32)
+		&& hero.y <= (monster.y + 32)
+		&& monster.y <= (hero.y + 32)
+	) {
+		++monstersCaught;
+		reset();
+	}
+};
 
